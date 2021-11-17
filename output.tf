7d596930a92086a7156d4a389f39ea2219792dc9
shivanandgante470 <shivanandgante470@gmail.com>
2021-11-17 22:46:05 +0530

    done

---
+++ output.tf
@@ -0,0 +1,39 @@
+output "local_resource_name" {
+    //value = azurerm_resource_group.rg.name
+    value   = local.resource_group_name
+}
+
+output "resource_id" {
+    value = azurerm_resource_group.rg.id
+}
+
+output "location" {
+    value = azurerm_resource_group.rg.location
+  
+}
+
+output "tags" {
+    value = azurerm_resource_group.rg.tags
+}
+
+output "list_type" {
+    value = var.var_list_type
+}
+
+output "tagss" {
+  value = var.tags
+}
+
+output "req_approval" {
+    value = var.require_approval
+}
+
+output "number" {
+    value = var.number_var
+  
+}
+
+output "car_model" {
+value   = var.car_model
+  
+}
---
+++ variable.tf
@@ -0,0 +1,63 @@
+
+variable "org_name" {
+  default = "BEE-Org"
+  description = "Name of Organization"
+}
+
+
+variable "project_name" {
+  default = "BEE-Project"
+  type = string
+}
+
+variable "environment_name" {
+  default = "Dev"
+}
+
+variable "environment_instance" {
+  default = "01"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "tags" {
+    type = map
+    default = {
+      Environment = "Development"
+      department  = "Account"
+  }
+}
+variable "var_list_type" {
+  type = list(string)
+  default = ["Alph", "1", "Beta", "$"]
+}
+
+variable "require_approval" {
+   type  = bool
+   default = false
+}
+
+variable "number_var" {
+  type  = number
+  default = 12345
+}
+
+variable "car_model" {
+	default	= {
+	  engine = {
+      "type" = "tuboboost"
+      "year"	= "2021"
+      "capacity" = "3000cc"
+	},
+	Range	= {
+	Start	= 10
+	End		= 200
+	},
+	maxSpeed	= 100,
+	milage	= "10/kmpl",
+	isThisSelfStart	= true,
+	availableModel 	= ["basic", "Automated", "Petrole", "G109"]
+  }
+}
