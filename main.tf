7f124dd34fdc93ec6a438a0e1448c8383a0715db
shivanandgante470 <shivanandgante470@gmail.com>
2021-11-17 19:09:39 +0530

    Added main tf file

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
-#added.data "" "name" {
-  
+provider "azurerm" {
+    features {} 
+}
+
+locals {
+  resource_group_name = format("%s-%s-%s-%s", var.org_name,var.project_name,var.environment_name,var.environment_instance)
+}
+
+# format -- means its a another variable
+resource "azurerm_resource_group" "rg"{
+    name = local.resource_group_name
+    location = var.location
+    tags = var.tags
+
+}
+
